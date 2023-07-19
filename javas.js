8954bb0842c580dfd68ffef3d6eb49c99e60ff83
aditya-jindal <139961475+aditya-jindal@users.noreply.github.com>
2023-07-19 18:02:06 +0530

    updated code to add time, zoom functionality, etc.

--- javas.js
+++ javas.js
@@ -1,20 +1,41 @@
-function toggleList(listId) {
+function updateTime() {
+    var dt = new Date();
+    document.getElementById("datetime").innerHTML = (("0" + dt.getDate()).slice(-2)) + "-" + (("0" + (dt.getMonth() + 1)).slice(-2)) + "-" + (dt.getFullYear()) + " [" + (("0" + dt.getHours()).slice(-2)) + ":" + (("0" + dt.getMinutes()).slice(-2)) + ":" + (("0" + dt.getSeconds()).slice(-2)) +"]";
+  }
+  updateTime();
+  setInterval(updateTime, 1000);
+  function increaseFontSize() {
+    var rootElement = document.documentElement;
+    var currentSize = parseFloat(window.getComputedStyle(rootElement, null).getPropertyValue('font-size'));
+    rootElement.style.fontSize = (currentSize + 1) + "px";
+  }
+
+  function resetFontSize() {
+    document.documentElement.style.fontSize = "16px"; // Set the default font size
+  }
+
+  function decreaseFontSize() {
+    var rootElement = document.documentElement;
+    var currentSize = parseFloat(window.getComputedStyle(rootElement, null).getPropertyValue('font-size'));
+    rootElement.style.fontSize = (currentSize - 1) + "px";
+  }
+function toggleList(listId,arrowId) {
     var list = document.getElementById(listId);
-    if (list.style.display === "none") {
+    var arrow = document.getElementById(arrowId);
+    if (list.style.display === "none" || list.style.display === "") {
       list.style.display = "block";
+      arrow.classList.add("arrow-up");
+
     } else {
       list.style.display = "none";
+      arrow.classList.remove("arrow-up");
+
     }
   }
-  function displayFormattedTime() {
-    var currentDate = new Date();
-    var day = currentDate.getDate();
-    var month = currentDate.toLocaleString('default', { month: 'short' });
-    var year = currentDate.getFullYear();
-    var hours = currentDate.getHours().toString().padStart(2, '0');
-    var minutes = currentDate.getMinutes().toString().padStart(2, '0');
-    var seconds = currentDate.getSeconds().toString().padStart(2, '0');
-    var formattedTime = day + '-' + month + '-' + year + ' [' + hours + ':' + minutes + ':' + seconds + ']';
-    var timeDisplay = document.getElementById("timeDisplay");
-    timeDisplay.innerHTML = formattedTime;
-  }
+
+  window.addEventListener("DOMContentLoaded", function() {
+    var hiddenLists = document.getElementsByClassName("hidden");
+    for (var i = 0; i < hiddenLists.length; i++) {
+      hiddenLists[i].style.display = "none";
+    }
+  });
