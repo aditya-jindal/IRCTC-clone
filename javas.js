6ccd11de716d14f5f45004689dc9d41e15194439
aditya-jindal <139961475+aditya-jindal@users.noreply.github.com>
2023-07-19 14:29:01 +0530

    Add files via upload

---
+++ javas.js
@@ -0,0 +1,8 @@
+function toggleList(listId) {
+    var list = document.getElementById(listId);
+    if (list.style.display === "none") {
+      list.style.display = "block";
+    } else {
+      list.style.display = "none";
+    }
+  }
