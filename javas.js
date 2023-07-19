320b6249fcd6dbbfd3fa951f7c0f517d31504875
aditya-jindal <139961475+aditya-jindal@users.noreply.github.com>
2023-07-19 15:31:39 +0530

    Add files via upload

--- javas.js
+++ javas.js
@@ -6,3 +6,15 @@ function toggleList(listId) {
       list.style.display = "none";
     }
   }
+  function displayFormattedTime() {
+    var currentDate = new Date();
+    var day = currentDate.getDate();
+    var month = currentDate.toLocaleString('default', { month: 'short' });
+    var year = currentDate.getFullYear();
+    var hours = currentDate.getHours().toString().padStart(2, '0');
+    var minutes = currentDate.getMinutes().toString().padStart(2, '0');
+    var seconds = currentDate.getSeconds().toString().padStart(2, '0');
+    var formattedTime = day + '-' + month + '-' + year + ' [' + hours + ':' + minutes + ':' + seconds + ']';
+    var timeDisplay = document.getElementById("timeDisplay");
+    timeDisplay.innerHTML = formattedTime;
+  }
