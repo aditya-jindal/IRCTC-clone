212650de104a07848ad74add4373b822fe7cd245
aditya-jindal <139961475+aditya-jindal@users.noreply.github.com>
2023-07-20 14:04:45 +0530

    Added arrows that change when list expands

--- javas.js
+++ javas.js
@@ -1,41 +1,43 @@
 function updateTime() {
-    var dt = new Date();
-    document.getElementById("datetime").innerHTML = (("0" + dt.getDate()).slice(-2)) + "-" + (("0" + (dt.getMonth() + 1)).slice(-2)) + "-" + (dt.getFullYear()) + " [" + (("0" + dt.getHours()).slice(-2)) + ":" + (("0" + dt.getMinutes()).slice(-2)) + ":" + (("0" + dt.getSeconds()).slice(-2)) +"]";
-  }
+  var dt = new Date();
+  document.getElementById("datetime").innerHTML = (("0" + dt.getDate()).slice(-2)) + "-" + (("0" + (dt.getMonth() + 1)).slice(-2)) + "-" + (dt.getFullYear()) + " [" + (("0" + dt.getHours()).slice(-2)) + ":" + (("0" + dt.getMinutes()).slice(-2)) + ":" + (("0" + dt.getSeconds()).slice(-2)) +"]";//made by Aditya Jindal
+}
   updateTime();
   setInterval(updateTime, 1000);
-  function increaseFontSize() {
-    var rootElement = document.documentElement;
-    var currentSize = parseFloat(window.getComputedStyle(rootElement, null).getPropertyValue('font-size'));
-    rootElement.style.fontSize = (currentSize + 1) + "px";
-  }
 
-  function resetFontSize() {
+function increaseFontSize() {
+  var rootElement = document.documentElement;
+  var currentSize = parseFloat(window.getComputedStyle(rootElement, null).getPropertyValue('font-size'));
+  rootElement.style.fontSize = (currentSize + 1) + "px";
+}
+
+function resetFontSize() {
     document.documentElement.style.fontSize = "16px"; // Set the default font size
-  }
+}
 
-  function decreaseFontSize() {
+function decreaseFontSize() {
     var rootElement = document.documentElement;
     var currentSize = parseFloat(window.getComputedStyle(rootElement, null).getPropertyValue('font-size'));
     rootElement.style.fontSize = (currentSize - 1) + "px";
-  }
-function toggleList(listId,arrowId) {
-    var list = document.getElementById(listId);
-    var arrow = document.getElementById(arrowId);
-    if (list.style.display === "none" || list.style.display === "") {
-      list.style.display = "block";
-      arrow.classList.add("arrow-up");
 
-    } else {
-      list.style.display = "none";
-      arrow.classList.remove("arrow-up");
-
-    }
   }
 
-  window.addEventListener("DOMContentLoaded", function() {
-    var hiddenLists = document.getElementsByClassName("hidden");
-    for (var i = 0; i < hiddenLists.length; i++) {
-      hiddenLists[i].style.display = "none";
-    }
-  });
+function toggleList(listId,buttonId) {
+  var list = document.getElementById(listId);
+  var button = document.getElementById(buttonId);
+  if (list.style.display === "none" || list.style.display === "") {
+    list.style.display = "block";
+    button.textContent = "▴";
+
+  } else {
+    list.style.display = "none";
+    button.textContent = "▾";
+  }
+}
+
+window.addEventListener("DOMContentLoaded", function() {
+  var hiddenLists = document.getElementsByClassName("hidden");
+  for (var i = 0; i < hiddenLists.length; i++) {
+    hiddenLists[i].style.display = "none";
+  }
+});
